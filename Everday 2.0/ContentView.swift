1622ddbaf273a991c282261e3005f994c615a1df
Nour <nalsramah@gmail.com>
2025-02-07 23:39:10 -0600

    Initial Commit

---
+++ Everday 2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Everday 2.0
+//
+//  Created by Nour Alsramah on 2/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Everday 2.0/Everday_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Everday_2_0App.swift
+//  Everday 2.0
+//
+//  Created by Nour Alsramah on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Everday_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
